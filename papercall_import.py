90101bacd3bf861f0b6bb47f44deac65f324b497
Jeff Triplett <jeff.triplett@gmail.com>
2019-05-29 16:08:59 -0500

    :shirt: Lints via Black

--- papercall_import.py
+++ papercall_import.py
@@ -7,12 +7,11 @@ from slugify import slugify
 from xlwt import easyxf, Workbook
 
 # Possible submission states
-SUBMISSION_STATES = ('submitted', 'accepted', 'rejected', 'waitlist')
+SUBMISSION_STATES = ("submitted", "accepted", "rejected", "waitlist")
 
 # Style for the Spreadsheet headers
 HEADER_STYLE = easyxf(
-    'font: name Verdana, color-index blue, bold on',
-    num_format_str='#,##0.00'
+    "font: name Verdana, color-index blue, bold on", num_format_str="#,##0.00"
 )
 
 
@@ -20,10 +19,12 @@ def get_api_key():
     """
     Get the user's API key
     """
-    print('Your DjangoCon PaperCall API Key can be found here: https://www.papercall.io/events/2198/apidocs')
-    api_key = input('Please enter your PaperCall event API Key: ')
+    print(
+        "Your DjangoCon PaperCall API Key can be found here: https://www.papercall.io/events/2198/apidocs"
+    )
+    api_key = input("Please enter your PaperCall event API Key: ")
     if len(api_key) != 32:
-        raise ValueError('Error: API Key must be 32 characters long.')
+        raise ValueError("Error: API Key must be 32 characters long.")
 
     return api_key
 
@@ -32,11 +33,11 @@ def get_format():
     """
     Get the output format to write to.
     """
-    print('Which format would you like to output?')
-    print('1: Excel')
-    print('2: YAML/Markdown for Jekyll')
-    file_format = input('Please enter your your output format (1 or 2): ')
-    if file_format not in ('1', '2'):
+    print("Which format would you like to output?")
+    print("1: Excel")
+    print("2: YAML/Markdown for Jekyll")
+    file_format = input("Please enter your your output format (1 or 2): ")
+    if file_format not in ("1", "2"):
         raise ValueError('Error: Output format must be "1" or "2".')
 
     return file_format
@@ -60,13 +61,9 @@ def create_excel(api_key, xls_file):
     total_feedback = 0
 
     # Get the event ID number
-    r = get(
-        'https://www.papercall.io/api/v1/event?_token={0}'.format(
-            api_key,
-        )
-    )
+    r = get("https://www.papercall.io/api/v1/event?_token={0}".format(api_key))
 
-    event_id = r.json()['cfp']['id']
+    event_id = r.json()["cfp"]["id"]
 
     # Create the Spreadsheet Workbook
     wb = Workbook()
@@ -78,8 +75,17 @@ def create_excel(api_key, xls_file):
 
         # Create the new sheet and header row for each talk state
         ws = wb.add_sheet(submission_state.upper())
-        columns = ['Link', 'Title', 'Format', 'Audience', 'Rating', 'Trust',
-                   'Name', 'Email', 'Bio']
+        columns = [
+            "Link",
+            "Title",
+            "Format",
+            "Audience",
+            "Rating",
+            "Trust",
+            "Name",
+            "Email",
+            "Bio",
+        ]
 
         # Write a header row
         for col_num, col_name in enumerate(columns):
@@ -87,62 +93,66 @@ def create_excel(api_key, xls_file):
 
         col_num += 1
         for x in range(col_num, 35):
-            ws.write(0, x, 'Comments / Feedback {}'.format(x - col_num + 1), HEADER_STYLE)
+            ws.write(
+                0, x, "Comments / Feedback {}".format(x - col_num + 1), HEADER_STYLE
+            )
 
         r = get(
-            'https://www.papercall.io/api/v1/submissions?_token={0}&state={1}&per_page=1000'.format(
-                api_key,
-                submission_state,
+            "https://www.papercall.io/api/v1/submissions?_token={0}&state={1}&per_page=1000".format(
+                api_key, submission_state
             )
         )
 
         for submission in r.json():
+            print(submission)
             total_submissions += 1
 
-            ws.write(num_row, 0, f"https://www.papercall.io/cfps/{event_id}/submissions/{submission['id']}")
-            ws.write(num_row, 1, submission['talk']['title'])
-            ws.write(num_row, 2, submission['talk']['talk_format'])
-            ws.write(num_row, 3, submission['talk']['audience_level'])
-            ws.write(num_row, 4, '{0:.4g}'.format(submission['rating']))
-            ws.write(num_row, 5, '{0:.4g}'.format(submission['trust']))
+            ws.write(
+                num_row,
+                0,
+                f"https://www.papercall.io/cfps/{event_id}/submissions/{submission['id']}",
+            )
+            ws.write(num_row, 1, submission["talk"]["title"])
+            ws.write(num_row, 2, submission["talk"]["talk_format"])
+            ws.write(num_row, 3, submission["talk"]["audience_level"])
+            ws.write(num_row, 4, "{0:.4g}".format(submission["rating"]))
+            ws.write(num_row, 5, "{0:.4g}".format(submission["trust"]))
 
-            if 'profile' in submission:
-                ws.write(num_row, 6, submission['profile']['name'])
-                ws.write(num_row, 7, submission['profile']['email'])
-                ws.write(num_row, 8, submission['profile']['bio'])
+            if "profile" in submission:
+                ws.write(num_row, 6, submission["profile"]["name"])
+                ws.write(num_row, 7, submission["profile"]["email"])
+                ws.write(num_row, 8, submission["profile"]["bio"])
             else:
-                ws.write(num_row, 6, 'Not Revealed')
-                ws.write(num_row, 7, 'Not Revealed')
-                ws.write(num_row, 8, 'Not Revealed')
+                ws.write(num_row, 6, "Not Revealed")
+                ws.write(num_row, 7, "Not Revealed")
+                ws.write(num_row, 8, "Not Revealed")
 
             col_num_count = col_num
 
             # Only include ratings comments if they've been entered
             c = get(
-                'https://www.papercall.io/api/v1/submissions/{}/ratings?_token={}'.format(
-                    submission['id'],
-                    api_key,
+                "https://www.papercall.io/api/v1/submissions/{}/ratings?_token={}".format(
+                    submission["id"], api_key
                 )
             )
             for ratings_comment in c.json():
                 total_ratings += 1
 
-                if len(ratings_comment['comments']):
+                if len(ratings_comment["comments"]):
                     ws.write(
                         num_row,
                         col_num_count,
-                        '(Comment from {}) {}'.format(
-                            ratings_comment['user']['email'],
-                            ratings_comment['comments'],
+                        "(Comment from {}) {}".format(
+                            ratings_comment["user"]["email"],
+                            ratings_comment["comments"],
                         ),
                     )
                     col_num_count += 1
 
             # Loop through all of the submitter / reviewer feedback and include after comments
             f = get(
-                'https://www.papercall.io/api/v1/submissions/{}/feedback?_token={}'.format(
-                    submission['id'],
-                    api_key,
+                "https://www.papercall.io/api/v1/submissions/{}/feedback?_token={}".format(
+                    submission["id"], api_key
                 )
             )
             for feedback in f.json():
@@ -150,9 +160,8 @@ def create_excel(api_key, xls_file):
                 ws.write(
                     num_row,
                     col_num_count,
-                    '(Feedback from {}) {}'.format(
-                        feedback['user']['email'],
-                        feedback['body'],
+                    "(Feedback from {}) {}".format(
+                        feedback["user"]["email"], feedback["body"]
                     ),
                 )
                 col_num_count += 1
@@ -167,77 +176,76 @@ def create_excel(api_key, xls_file):
 def create_yaml(api_key, yaml_dir):
     for submission_state in SUBMISSION_STATES:
         # Create the directories, if they don't exist.
-        makedirs(
-            '{}/{}'.format(
-                yaml_dir,
-                submission_state,
-            ), exist_ok=True,
-        )
+        makedirs("{}/{}".format(yaml_dir, submission_state), exist_ok=True)
 
         r = get(
-            'https://www.papercall.io/api/v1/submissions?_token={0}&state={1}&per_page=1000'.format(
-                api_key,
-                submission_state,
+            "https://www.papercall.io/api/v1/submissions?_token={0}&state={1}&per_page=1000".format(
+                api_key, submission_state
             )
         )
 
         for submission in r.json():
+            print(submission)
             talk_format = None
-            if submission['talk']['talk_format'][0:4].lower() == "talk":
+            if submission["talk"]["talk_format"][0:4].lower() == "talk":
                 talk_format = "talk"
-            elif submission['talk']['talk_format'][0:4].lower() == "tuto":
+            elif submission["talk"]["talk_format"][0:4].lower() == "tuto":
                 talk_format = "tutorial"
 
             if talk_format:
-                talk_title_slug = slugify(submission['talk']['title'])
+                talk_title_slug = slugify(submission["talk"]["title"])
 
-                post = frontmatter.loads(submission['talk']['description'])
-                post['abstract'] = submission['talk']['abstract']
-                post['category'] = talk_format
-                post['title'] = submission['talk']['title']
-                post['difficulty'] = submission['talk']['audience_level']
-                post['permalink'] = '/{}/{}/'.format(
-                    talk_format,
-                    talk_title_slug,
-                )
-                post['layout'] = 'session-details'
-                post['accepted'] = True if submission_state == 'accepted' else False
-                post['published'] = True
-                post['sitemap'] = True
+                post = frontmatter.loads(submission["talk"]["description"])
+                post["abstract"] = submission["talk"]["abstract"]
+                post["category"] = talk_format
+                post["title"] = submission["talk"]["title"]
+                post["difficulty"] = submission["talk"]["audience_level"]
+                post["permalink"] = "/{}/{}/".format(talk_format, talk_title_slug)
+                post["layout"] = "session-details"
+                post["accepted"] = True if submission_state == "accepted" else False
+                post["published"] = True
+                post["sitemap"] = True
 
                 # TODO: Scheduling info...
-                post['date'] = '2018-10-15 09:00'
-                post['room'] = ''
-                post['track'] = ''
+                post["date"] = "2018-10-15 09:00"
+                post["room"] = ""
+                post["track"] = ""
 
                 # TODO: Determine if we still need summary (I don't think we do)
-                post['summary'] = ''
+                post["summary"] = ""
 
                 # todo: refactor template layout to support multiple authors
-                post['presenters'] = [
+                post["presenters"] = [
                     {
-                        'name': submission['profile']['name'] if 'profile' in submission else 'No Profile',
-                        'bio': submission['profile']['bio'] if 'profile' in submission else 'No Profile',
-                        'company': submission['profile']['company'] if 'profile' in submission else 'No Profile',
-                        'photo_url': '',
-                        'github': '',
-                        'twitter': submission['profile']['twitter'] if 'profile' in submission else 'No Profile',
-                        'website': submission['profile']['url'] if 'profile' in submission else 'No Profile',
-                    },
+                        "name": submission["profile"]["name"]
+                        if "profile" in submission
+                        else "No Profile",
+                        "bio": submission["profile"]["bio"]
+                        if "profile" in submission
+                        else "No Profile",
+                        "company": submission["profile"]["company"]
+                        if "profile" in submission
+                        else "No Profile",
+                        "photo_url": "",
+                        "github": "",
+                        "twitter": submission["profile"]["twitter"]
+                        if "profile" in submission
+                        else "No Profile",
+                        "website": submission["profile"]["url"]
+                        if "profile" in submission
+                        else "No Profile",
+                    }
                 ]
 
                 # post conference info
-                post['video_url'] = ''
-                post['slides_url'] = ''
+                post["video_url"] = ""
+                post["slides_url"] = ""
 
                 with open(
-                    '{}/{}/{}-{}.md'.format(
-                        yaml_dir,
-                        submission_state,
-                        talk_format,
-                        talk_title_slug,
+                    "{}/{}/{}-{}.md".format(
+                        yaml_dir, submission_state, talk_format, talk_title_slug
                     ),
-                    'wb'
+                    "wb",
                 ) as file_to_write:
                     frontmatter.dump(post, file_to_write)
 
@@ -246,14 +254,18 @@ def create_yaml(api_key, yaml_dir):
 
 def main():
     try:
-        api_key = env('PAPERCALL_API_KEY')
+        api_key = env("PAPERCALL_API_KEY")
     except ConfigurationError:
         api_key = get_api_key()
     file_format = get_format()
 
     if file_format == "1":
-        xls_file = get_filename('Filename to write [djangoconus.xls]: ', 'djangoconus.xls')
-        total_submissions, total_ratings, total_feedback = create_excel(api_key, xls_file)
+        xls_file = get_filename(
+            "Filename to write [djangoconus.xls]: ", "djangoconus.xls"
+        )
+        total_submissions, total_ratings, total_feedback = create_excel(
+            api_key, xls_file
+        )
 
         print(
             """
@@ -267,7 +279,7 @@ def main():
             )
         )
     elif file_format == "2":
-        yaml_dir = get_filename('Directory to write to [yaml]: ', 'yaml')
+        yaml_dir = get_filename("Directory to write to [yaml]: ", "yaml")
         create_yaml(api_key, yaml_dir)
 
 
